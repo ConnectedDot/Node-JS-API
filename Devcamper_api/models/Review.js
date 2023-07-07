7641b38a1945a91ac6bdb22c2903632f1adc8ec6
ConnectedDot <psalmomo@gmail.com>
2023-07-07 02:51:15 +0100

    Initial commit

--- Devcamper_api/models/Review.js
+++ Devcamper_api/models/Review.js
@@ -1,56 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
   title: {
     type: String,
     trim: true,
-    required: [true, 'Please add a title for the review'],
-    maxlength: 100
+    required: [true, "Please add a title for the review"],
+    maxlength: 100,
   },
   text: {
     type: String,
-    required: [true, 'Please add some text']
+    required: [true, "Please add some text"],
   },
   rating: {
     type: Number,
     min: 1,
     max: 10,
-    required: [true, 'Please add a rating between 1 and 10']
+    required: [true, "Please add a rating between 1 and 10"],
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   bootcamp: {
     type: mongoose.Schema.ObjectId,
-    ref: 'Bootcamp',
-    required: true
+    ref: "Bootcamp",
+    required: true,
   },
   user: {
     type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: true
-  }
+    ref: "User",
+    required: true,
+  },
 });
 
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
 // Static method to get avg rating and save
-ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
   const obj = await this.aggregate([
     {
-      $match: { bootcamp: bootcampId }
+      $match: { bootcamp: bootcampId },
     },
     {
       $group: {
-        _id: '$bootcamp',
-        averageRating: { $avg: '$rating' }
-      }
-    }
+        _id: "$bootcamp",
+        averageRating: { $avg: "$rating" },
+      },
+    },
   ]);
 
- try {
+  try {
     if (obj[0]) {
       await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
         averageRating: obj[0].averageRating.toFixed(1),
@@ -60,19 +60,19 @@ ReviewSchema.statics.getAverageRating = async function(bootcampId) {
         averageRating: undefined,
       });
     }
-  }  catch (err) {
+  } catch (err) {
     console.error(err);
   }
 };
 
 // Call getAverageCost after save
-ReviewSchema.post('save', async function() {
+ReviewSchema.post("save", async function () {
   await this.constructor.getAverageRating(this.bootcamp);
 });
 
 // Call getAverageCost before remove
-ReviewSchema.post('remove', async function() {
+ReviewSchema.post("remove", async function () {
   await this.constructor.getAverageRating(this.bootcamp);
 });
 
-module.exports = mongoose.model('Review', ReviewSchema);
+module.exports = mongoose.model("Review", ReviewSchema);
