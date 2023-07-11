d3924a64bb433d75f196b65d68f8ceff526e3e87
Theophilus Samuel <psalmomo@gmail.com>
2023-07-11 01:36:35 +0100

    geocode coordinates

--- Devcamper_api/utils/geocoder.js
+++ Devcamper_api/utils/geocoder.js
@@ -1,10 +1,10 @@
-const NodeGeocoder = require('node-geocoder');
+const NodeGeocoder = require("node-geocoder");
 
 const options = {
   provider: process.env.GEOCODER_PROVIDER,
-  httpAdapter: 'https',
+  httpAdapter: "https",
   apiKey: process.env.GEOCODER_API_KEY,
-  formatter: null
+  formatter: null,
 };
 
 const geocoder = NodeGeocoder(options);
